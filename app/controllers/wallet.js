af9082c2da126bf084ec77cb1de647f27e0037f8
nathlauga <nathan.lauga@gmail.com>
2018-05-02 10:47:29 +0200

    Add : controlers user, trading, wallet

---
+++ app/controllers/wallet.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Wallet = mongoose.model('Wallet');
+const User = mongoose.model('User');
+
+exports.index = (req, res) => {
+    User.findById(req.session.id, function (err, user) {
+        let wallet = user.wallet;
+        res.send(wallet);
+    });
+};
