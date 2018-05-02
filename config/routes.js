2b70ac31eca208917dd607fa949e26cbcb574997
Unknown <axel.denjean@gmail.com>
2018-05-02 12:16:45 +0200

    New route login

--- config/routes.js
+++ config/routes.js
@@ -8,6 +8,7 @@ const home = require('../app/controllers/home');
 const market = require('../app/controllers/market');
 const wallet = require('../app/controllers/wallet');
 const signUp = require('../app/controllers/signUp');
+const login = require('../app/controllers/login');
 
 
 /**
@@ -20,6 +21,7 @@ module.exports = function (app, passport) {
   app.get('/market', market.index);
   app.get('/wallet', wallet.index);
   app.get('/signUp', signUp.index);
+  app.get('/login', login.index);
 
   /**
    * Error handling
